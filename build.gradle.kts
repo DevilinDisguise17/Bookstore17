440b6dd4ab1b8006344818604b1cba65ded14ae5
DevilinDisguise17 <diogo.miguelmartins@my.istec.pt>
2024-12-31 10:38:38 +0000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,28 @@
-plugins {
-    kotlin("jvm") version "2.0.10"
+import com.google.-firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import com.google.auth.oauth2.GoogleCredentials
+import java.io.FileInputStream
+
+// Initialize Firebase
+fun initializeFirebase() {
+    try {
+        val options = FirebaseOptions.builder()
+            .setCredentials(GoogleCredentials.fromStream(FileInputStream("/users/diogomartins/ideaprojects/bookstore/resources/books-4269f-default-rtdb-export.json")))
+            .setDatabaseUrl("https://books-4269f-default-rtdb.europe-west1.firebasedatabase.app")
+            .build()
+
+        FirebaseApp.initializeApp(options)
+        println("Firebase initialized successfully.")
+    } catch (e: Exception) {
+        e.printStackTrace()
+        println("Error initializing Firebase.")
+    }
 }
 
-group = "Bookstore"
-version = "1.0-SNAPSHOT"
+// Main function to test Firebase
+fun main() {
+    // Initialize Firebase
+    initializeFirebase()
 
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
+    // Add your Firebase-related logic here, such as interacting with Firestore, Realtime Database, etc.
 }
